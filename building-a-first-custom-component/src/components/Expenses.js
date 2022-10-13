161b5572985deed28508a8fe20e12d66930bca4a
tolbastue <67708771+tolbastue@users.noreply.github.com>
2022-10-13 12:56:13 +0200

    refactored, added Card component

--- building-a-first-custom-component/src/components/Expenses.js
+++ building-a-first-custom-component/src/components/Expenses.js
@@ -1,13 +1,14 @@
 import "./Expenses.css";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.items.map((item) => (
         <ExpenseItem date={item.date} title={item.title} amount={item.amount} />
       ))}
-    </div>
+    </Card>
   );
 }
 
