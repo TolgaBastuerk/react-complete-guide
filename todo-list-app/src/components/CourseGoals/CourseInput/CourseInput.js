7a06540ccff8ecf06fb80c03334e2c4550a17440
Tolga Bastürk <tolga.bastuerk@outlook.com>
2022-10-28 10:45:13 +0200

    refactored styling to use css modules and fixed bugs

--- todo-list-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ todo-list-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import styled from "styled-components"
+import React, { useState, useEffect } from 'react';
+// import styled from "styled-components"
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => props.invalid ? 'red' : 'black'}
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => props.invalid ? 'red' : 'black'}
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => props.invalid ? 'red' : '3ccc'};
-    background: ${props => props.invalid ? '#ffd7d7' : 'transparent'}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => props.invalid ? 'red' : '3ccc'};
+//     background: ${props => props.invalid ? '#ffd7d7' : 'transparent'}
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -48,14 +48,18 @@ const CourseInput = props => {
       return;
     }
     props.onAddGoal(enteredValue);
+    setEnteredValue('');
+    document.getElementById("myForm").reset();
   };
 
+  useEffect( () => {},[])
+
   return (
-    <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+    <form onSubmit={formSubmitHandler} id="myForm">
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
